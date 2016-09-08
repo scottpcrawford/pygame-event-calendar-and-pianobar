1c50ca20a1a4c9ddd78e3807f831f06bdf257d69
scottpcrawford <scott@crawhouse.com>
2016-09-08 12:09:56 -0400

    Added to API Keys Example File

--- api_keys.py
+++
@@ -1,3 +0,0 @@
-
-open_weather_map_api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # replace this with your own open weather map api key
-google_calendar_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx@group.calendar.google.com' # replace this with the name of the google calendar you want to open
---
+++ api_keys_example.py
@@ -0,0 +1,5 @@
+open_weather_map_api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+google_calendar_id = 'xxxxxxxxxxxxxxxxxxxx@group.calendar.google.com'
+google_api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+wordnik_api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+forecastio_api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
